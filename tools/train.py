ff372e5c2efd9b75633ea697fd60ac5928eb8e1b
Your Name <you@example.com>
2023-06-26 16:19:45 +0800

    modified

--- tools/train.py
+++ tools/train.py
@@ -14,7 +14,7 @@ from mmdet.utils import setup_cache_size_limit_of_dynamo
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Train a detector')
-    parser.add_argument('config', help='train config file path')
+    parser.add_argument('--config', help='train config file path')
     parser.add_argument('--work-dir', help='the dir to save logs and models')
     parser.add_argument(
         '--amp',
@@ -61,6 +61,8 @@ def parse_args():
 
 def main():
     args = parse_args()
+    
+    args.config = r'myconfigs\study-configs\faster-rcnn_r50_fpn_1x_voc0712.py'
 
     # Reduce the number of repeated compilations and improve
     # training speed.
